38662369013dc97a646f367de3ac46b79c8cd5b9
nikejune <dmlgus322@hanyang.ac.kr>
2018-04-05 13:24:32 +0900

    Initial Commit

---
+++ AI_proj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AI_proj1
+//
+//  Created by nikejune on 2018. 4. 5..
+//  Copyright © 2018년 nikejune. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
